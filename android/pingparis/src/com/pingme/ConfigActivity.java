c4a8e1397f75684b380b5915956746e5896d3b62
Alexis Agahi <alag@users.sf.net>
2012-02-18 05:12:22 +0100

    Add Service with notification

--- android/pingparis/src/com/pingme/ConfigActivity.java
+++ android/pingparis/src/com/pingme/ConfigActivity.java
@@ -1,6 +1,7 @@
 package com.pingme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ConfigActivity extends Activity {
@@ -9,5 +10,9 @@ public class ConfigActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Intent serviceIntent = new Intent( "PING_USER_ACTION" );
+        serviceIntent.setClassName( this, "com.pingme.PingMeService" );
+        startService( serviceIntent );
     }
 }
